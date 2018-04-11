771610aef76918642cfbc17ec753d0ead904c515
Naveen <Chnaveen7496@gmail.com>
2018-04-11 19:12:54 +0530

    Added Replies Tableview ANimation in YTplayer

---
+++ SamplePageMenu/AllRepliesHeaderTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  AllRepliesHeaderTVCell.swift
+//  Telugu Churches
+//
+//  Created by N@n!'$ Mac on 11/04/18.
+//  Copyright © 2018 Mac OS. All rights reserved.
+//
+
+import UIKit
+
+class AllRepliesHeaderTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
